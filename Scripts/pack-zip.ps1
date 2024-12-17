5b98ce71e1be530f1b2604a3ab07c0fc2ff27e92
ema <mccoy39082@163.com>
2024-12-17 22:04:52 +0800

    New build script

--- Scripts/pack-zip.ps1
+++ Scripts/pack-zip.ps1
@@ -1,5 +1,5 @@
 Remove-Item ..\QuickLook.Plugin.OfficeViewer.qlplugin -ErrorAction SilentlyContinue
 
-$files = Get-ChildItem -Path ..\bin\Release\ -Exclude *.pdb,*.xml
+$files = Get-ChildItem -Path ..\Build\Release\ -Exclude *.pdb,*.xml
 Compress-Archive $files ..\QuickLook.Plugin.OfficeViewer.zip
 Move-Item ..\QuickLook.Plugin.OfficeViewer.zip ..\QuickLook.Plugin.OfficeViewer.qlplugin
